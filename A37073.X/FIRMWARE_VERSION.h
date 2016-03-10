b6cc5bf905f5c08228285c37765f28c5b48a376d
hwanetick <hwanetick@etm-inc.local>
2016-03-09 17:02:10 -0800

    91-37073-000 Rev 1.0.0
    For Agile

--- A37073.X/FIRMWARE_VERSION.h
+++ A37073.X/FIRMWARE_VERSION.h
@@ -8,9 +8,9 @@
 #ifndef FIRMWARE_VERSION_H
 #define	FIRMWARE_VERSION_H
 
-#define FIRMWARE_AGILE_REV     0
+#define FIRMWARE_AGILE_REV     1
 #define FIRMWARE_BRANCH        0
-#define FIRMWARE_MINOR_REV     3
+#define FIRMWARE_MINOR_REV     0
 
 #endif	/* FIRMWARE_VERSION_H */
 
